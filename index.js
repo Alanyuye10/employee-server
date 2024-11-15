28853dbc99edeb47e065763a6651c14927926739
Alan Yuye <alanyuyealan@gmail.com>
2024-11-15 16:05:18 +0530

    new commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 
 const MPServer = jsonServer.create()
 const middleware = jsonServer.defaults()
-const PORT = 4000
+const PORT = 3000
 const route = jsonServer.router('db.json')
 
 MPServer.use(middleware)
